a1f0c2ef33e42482d273613fcc3925a2364e758c
EmmaGS <mail@emmagiverny.co.uk>
2024-03-22 12:28:40 +0000

    Add filter and searches

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -36,7 +36,6 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
-    search_fields = ('email')
 
     objects = UserProfileManager()
 
